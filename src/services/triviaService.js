fb1d461fadb6736df5c1ab27719dd092dabfc179
m00zy <maxwellzhu123@gmail.com>
2024-08-23 16:02:23 +1200

    implement fetchQuestions api call

--- src/services/triviaService.js
+++ src/services/triviaService.js
@@ -17,3 +17,21 @@ export async function fetchCategories() {
         return null;
     }
 }
+
+export async function fetchQuestions(numQuestions, category, difficulty) {
+    const url = BASE_URL + `/api.php?amount=${numQuestions}&category=${category}&difficulty=${difficulty}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json();
+        return data.trivia_categories.results;
+        
+    }
+    catch (error) {
+        console.error('Error fetching trivia categories:', error);
+        return null;
+    }
+}
