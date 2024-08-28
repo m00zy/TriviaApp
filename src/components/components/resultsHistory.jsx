2c41a34631a1feaa534fbcaf240e7d93b23e3488
m00zy <maxwellzhu123@gmail.com>
2024-08-28 20:08:09 +1200

    add table to show results history

--- src/components/components/resultsHistory.jsx
+++ src/components/components/resultsHistory.jsx
@@ -5,6 +5,30 @@ export default function ResultsHistory() {
     const { results } = useContext(QuizContext);
 
     return (
-        <p>{results.map(result => result.score)}</p>
+        <div className='results-history-container'>
+            <p>Previous Results</p>
+            <div className='results-history'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <th>Difficulty</th>
+                        <th>Number of Questions</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {results.map((result, index) => (
+                        <tr key={index}>
+                            <td>{result.category}</td>
+                            <td>{result.difficulty}</td>
+                            <td>{result.num_questions}</td>
+                            <td>{result.score}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+        </div>
     );
 }
