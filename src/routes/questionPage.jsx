9152d4046a2360643372ed00b82b1e3f63130cc4
m00zy <maxwellzhu123@gmail.com>
2024-08-30 21:40:22 +1200

    remove title case function

--- src/routes/questionPage.jsx
+++ src/routes/questionPage.jsx
@@ -63,14 +63,6 @@ export default function QuestionPage() {
         return <p>Loading...</p>;
     }
 
-    const titleCase = (str) => {
-        var splitStr = str.toLowerCase().split(' ');
-        for (var i = 0; i < splitStr.length; i++) {
-            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-        }
-        return splitStr.join(' '); 
-    }
-
     const currQuestion = questions[questionIndex];
     
     const handleSubmit = (e) => {
